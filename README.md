29a08a51694de8ed30411ba83dc71c67d6b350f0
Kevin Ngo <me@ngokevin.com>
2018-03-25 12:26:12 -0700

    generator

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # gunters-of-oasis
 
 Gunters (Egg Hunters) of OASIS (Open and Super Immersive Simulation).
+
+## Development
+
+Run server.
+
+```
+npm run start
+```
+
+Generate OASIS. Template of sectors at `src/index.html` and generator at
+`generator/index.js`. Need to have a home sector.
+
+```
+npm run generate
+```
--- generator/index.js
+++ generator/index.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var nunjucks = require('nunjucks');
 
+var template = fs.readFileSync('./src/index.html', 'utf-8');
+
 var SECTORS = [
   {
     environmentType: 'forest',
@@ -12,6 +14,22 @@ var SECTORS = [
   }
 ];
 
-SECTORS.forEach(() => {
-
+SECTORS.forEach(sector => {
+  var html;
+  var seed;
+  seed = randomId();
+  html = nunjucks.renderString(template, {
+    environment: `preset: ${sector.environmentType}; seed: ${seed}`
+  });
+  fs.writeFileSync(`oasis/${seed}.html`, html);
 });
+
+function randomId () {
+	var i;
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (i = 0; i < 5; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}

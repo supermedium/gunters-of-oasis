a5255fb2b8ff1c6d7ae2754fe12d3c46b2a3f499
Kevin Ngo <me@ngokevin.com>
2018-03-28 12:15:51 -0700

    cut more

--- src/components/oasis-portal.js
+++ src/components/oasis-portal.js
@@ -1,68 +1,5 @@
-/**
- * Portal. Modified from link component.
- */
-AFRAME.registerComponent('oasis-portal', {
-  schema: {
-    backgroundColor: {default: 'red', type: 'color'},
-    borderColor: {default: 'white', type: 'color'},
-    highlighted: {default: false},
-    highlightedColor: {default: '#24CAFF', type: 'color'},
-    href: {default: ''},
-    image: {type: 'asset'},
-    on: {default: 'click'},
-    title: {default: ''},
-    titleColor: {default: 'white', type: 'color'},
-    visualAspectEnabled: {default: true}
-  },
-
-  init: function () {
-    this.navigate = this.navigate.bind(this);
-    this.initVisualAspect();
-  },
-
-  update: function (oldData) {
-    var data = this.data;
-    var el = this.el;
-    var backgroundColor;
-    var strokeColor;
-
-    backgroundColor = data.highlighted ? data.highlightedColor : data.backgroundColor;
-    strokeColor = data.highlighted ? data.highlightedColor : data.borderColor;
-    el.setAttribute('material', 'backgroundColor', backgroundColor);
-    el.setAttribute('material', 'strokeColor', strokeColor);
-
-    if (!data.image || oldData.image === data.image) { return; }
-
-    el.setAttribute('material', 'pano',
-                    typeof data.image === 'string' ? data.image : data.image.src);
-  },
-
-  play: function () {
-    this.el.addEventListener(this.data.on, this.navigate);
-  },
-
-  initVisualAspect: function () {
-    var el = this.el;
-    var textEl;
-
-    if (!this.data.visualAspectEnabled) { return; }
-
-    textEl = this.textEl = this.textEl || document.createElement('a-entity');
-
-    // Set portal.
-    el.setAttribute('geometry', {primitive: 'circle', radius: 1.0, segments: 36});
-    el.setAttribute('material', {shader: 'oasisPortal', pano: this.data.image, side: 'double'});
-  },
-
-  navigate: function () {
-    window.location = this.data.href;
-  }
-});
-
-/* eslint-disable */
 AFRAME.registerShader('oasisPortal', {
   schema: {
-    borderEnabled: {default: 1.0, type: 'int', is: 'uniform'},
     backgroundColor: {default: 'red', type: 'color', is: 'uniform'},
     pano: {type: 'map', is: 'uniform'},
     strokeColor: {default: 'white', type: 'color', is: 'uniform'}
@@ -72,4 +9,3 @@ AFRAME.registerShader('oasisPortal', {
 
   fragmentShader: require('./shaders/oasisPortalFragment.glsl')
 });
-/* eslint-enable */
--- src/components/shaders/oasisPortalFragment.glsl
+++ src/components/shaders/oasisPortalFragment.glsl
@@ -1,10 +1,9 @@
 #define RECIPROCAL_PI2 0.15915494,
 uniform sampler2D pano;
-uniform vec3 strokeColor;
 uniform vec3 backgroundColor;
-uniform float borderEnabled;
-varying float vDistanceToCenter;
+uniform vec3 strokeColor;
 varying float vDistance;
+varying float vDistanceToCenter;
 varying vec3 vWorldPosition;
 
 void main() {
@@ -13,12 +12,12 @@ void main() {
 
   float borderThickness = 0.94;
 
-  sampleUV.y = saturate(direction.y * 0.5  + 0.5);
   sampleUV.x = atan(direction.z, -direction.x) * -RECIPROCAL_PI2 + 0.5;
+  sampleUV.y = saturate(direction.y * 0.5  + 0.5);
 
-  if (vDistanceToCenter > borderThickness && borderEnabled == 1.0) {
+  if (vDistanceToCenter > borderThickness) {
     gl_FragColor = vec4(strokeColor, 1.0);
   } else {
-    gl_FragColor = mix(texture2D(pano, sampleUV), vec4(backgroundColor, 1.0), clamp(pow((vDistance / 15.0), 2.0), 0.0, 1.0));
+    gl_FragColor = texture2D(pano, sampleUV);
   }
 }

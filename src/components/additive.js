7e17744e80eb7cd2ec4b5fad58c92e6292839673
Diego F. Goberna <diego@feiss.be>
2018-04-02 14:52:45 +0200

    improved portal look

--- src/components/additive.js
+++ src/components/additive.js
@@ -3,5 +3,8 @@ AFRAME.registerComponent('additive', {
     this.el.addEventListener('subobjectloaded', function(ev){
       ev.detail.material.blending = THREE.AdditiveBlending;
     });
+    this.el.addEventListener('model-loaded', function(ev){
+      ev.detail.model.material.blending = THREE.AdditiveBlending;
+    });
   }
 });

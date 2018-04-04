7bb5dee0ac1f50f79308a88bd9a83055ac1a67a8
Diego F. Goberna <diego@feiss.be>
2018-04-04 18:15:59 +0200

    added portal shader distorsion

--- src/components/shaders/oasisPortalFragment.glsl
+++ src/components/shaders/oasisPortalFragment.glsl
@@ -13,17 +13,23 @@ void main() {
   vec3 color;
   vec3 direction = normalize(vWorldPosition - cameraPosition);
   vec2 sampleUV;
+  vec2 sampleA;
+  vec2 sampleB;
 
   float borderThickness = 0.94;
 
   sampleUV.x = atan(direction.z, -direction.x) * -RECIPROCAL_PI2 + 0.5;
   sampleUV.y = saturate(direction.y * 0.5  + 0.5);
 
+  // wobble
+  sampleA = sampleUV + sin(time/1000.0 + sampleUV.x * 24.0 + sampleUV.y * 18.0) * 0.004;
+  sampleB = sampleUV + sin(time/1000.0 + sampleUV.x * 24.0 + sampleUV.y * 18.0) * 0.005;
+    
   // Opacity portal effect, positive sin wave from 0.5 to 1.0.
   alpha = sin(time / 800.0);
   alpha = (alpha + 1.0) / 2.0;
   alpha = mix(0.85, 1.0, alpha);
-  color = texture2D(pano, sampleUV).xyz;
+  color = vec3(texture2D(pano, sampleB).x, texture2D(pano, sampleA).yz);
   alpha = smoothstep(0.05, 1.0, 1.6 - vDistanceToCenter);
   gl_FragColor = vec4(color, alpha);
 
